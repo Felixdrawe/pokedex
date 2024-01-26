7e990d450ec014198d49b0e1869cdcf94cc98c34
Felixdrawe <131857058+Felixdrawe@users.noreply.github.com>
2024-01-26 16:13:41 -0600

    240126_1630

--- chart.js
+++ chart.js
@@ -1,31 +1,33 @@
 function renderChart(statsName, baseStats) {
   const ctx = document.getElementById('myChart').getContext('2d');
 
-   // Set global font options
-   Chart.defaults.font.family = 'Noto Sans, sans-serif';
-   Chart.defaults.font.weight = 'bold'; // Set font weight to bold
-   Chart.defaults.font.color = 'black';
+  // Set global font options
+  Chart.defaults.font.family = 'Noto Sans, sans-serif';
+  Chart.defaults.font.weight = 'bold'; // Set font weight to bold
+  Chart.defaults.font.color = 'black';
 
   const config = {
-    type: 'bar', // Use 'bar' for both vertical and horizontal bar charts
+    type: 'bar',
     data: {
       labels: statsName,
-      datasets: [{
-        label: '',
-        data: baseStats,
-        backgroundColor: [
-          'rgba(255, 99, 132, 1)',
-          'rgba(54, 162, 235, 1)',
-          'rgba(255, 206, 86, 1)',
-          'rgba(75, 192, 192, 1)',
-          'rgba(153, 102, 255, 1)',
-          'rgba(255, 159, 64, 1)'
-        ],
-       
-          borderColor: 'black', 
-        
-        borderWidth: 1
-      }]
+      datasets: [
+        {
+          label: '',
+          data: baseStats,
+          backgroundColor: [
+            'rgba(255, 99, 132, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)',
+          ],
+
+          borderColor: 'black',
+
+          borderWidth: 1,
+        },
+      ],
     },
     options: {
       indexAxis: 'y', // Set the index axis to 'y' for horizontal bar chart
@@ -36,10 +38,10 @@ function renderChart(statsName, baseStats) {
             color: 'black',
             font: {
               family: 'Noto Sans, sans-serif',
-              size: 14 // Adjust font size as needed
-            }
-          }
-        }
+              size: 14, // Adjust font size as needed
+            },
+          },
+        },
       },
       scales: {
         x: {
@@ -48,10 +50,10 @@ function renderChart(statsName, baseStats) {
             display: true,
             font: {
               family: 'Noto Sans, sans-serif',
-              size: 14 // Adjust font size as needed
-            }
+              size: 14, // Adjust font size as needed
+            },
           },
-          beginAtZero: true
+          beginAtZero: true,
         },
         y: {
           ticks: {
@@ -59,11 +61,11 @@ function renderChart(statsName, baseStats) {
             display: true,
             font: {
               family: 'Noto Sans, sans-serif',
-              size: 14 // Adjust font size as needed
-            }
-          }
-        }
-      }
+              size: 14, // Adjust font size as needed
+            },
+          },
+        },
+      },
     },
   };
 
